32277b4ef58a148e81e0ff7024165680f8bf587b
Foggy-J <16501241+Foggy-J@users.noreply.github.com>
2025-11-28 16:57:18 +0000

    Fix Cloudflare DNS zone name error

--- cloudflare/cloudflare-dns-record/10-record.tf
+++ cloudflare/cloudflare-dns-record/10-record.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_dns_record" "dns_record" {
   zone_id = data.cloudflare_zone.zone.zone_id
-  name    = "${var.dns_record_name}.${var.cloudflare_zone.name}"
+  name    = "${var.dns_record_name}.${data.cloudflare_zone.zone.name}"
   type    = var.dns_record_type
   ttl     = var.dns_record_ttl
   proxied = var.dns_record_proxied
